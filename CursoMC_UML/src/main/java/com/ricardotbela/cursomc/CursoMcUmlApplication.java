02ddb2ea71ecbd104383c8fa5997de0e20b671cd
Ricardo Tosi Bela <69743502+Rjasao@users.noreply.github.com>
2022-10-26 21:07:47 -0300

    Estado e Cidade

--- CursoMC_UML/src/main/java/com/ricardotbela/cursomc/CursoMcUmlApplication.java
+++ CursoMC_UML/src/main/java/com/ricardotbela/cursomc/CursoMcUmlApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ricardotbela.cursomc.domain.Categoria;
+import com.ricardotbela.cursomc.domain.Cidade;
+import com.ricardotbela.cursomc.domain.Estado;
 import com.ricardotbela.cursomc.domain.Produto;
 import com.ricardotbela.cursomc.repositories.CategoriaRepository;
+import com.ricardotbela.cursomc.repositories.CidadeRepository;
+import com.ricardotbela.cursomc.repositories.EstadoRepository;
 import com.ricardotbela.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursoMcUmlApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcUmlApplication.class, args);
@@ -44,6 +54,21 @@ public class CursoMcUmlApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
 		
 	}
 }
